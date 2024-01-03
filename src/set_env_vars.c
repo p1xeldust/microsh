6cffa0c9f26ec306d585afce176ea3b8aa79dc8d
Pixel Dust <pixeldustp@gmail.com>
2024-01-03 12:18:46 +0300

    modified:   src/set_env_vars.c

--- src/set_env_vars.c
+++ src/set_env_vars.c
@@ -5,8 +5,8 @@
 
 void set_env_var(char* input) {
     char* var_name = strtok(input, "=");
-    char* var_value;
-    if ((var_value = strtok(NULL, "=")) != NULL) {
+    char* var_value = strtok(NULL, "=");
+    if (var_value != NULL) {
         setenv(var_name, var_value, 1);
     } else {
         setenv(var_name, "", 1);
